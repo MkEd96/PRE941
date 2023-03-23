61ac364d6b74ec64644ae12b6722840001dff0ff
MkEd96 <93858373+MkEd96@users.noreply.github.com>
2023-03-23 00:50:12 -0600

    Add files via upload

---
+++ Guia 8_EJ1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+#include<conio.h>
+#include<stdlib.h>
+
+int main() {
+    int numeros[12];
+    int pares;
+    int n;
+    int rangoA;
+    int rangoB;
+
+    for (int i = 0; i < 12; i++) {
+        cout << "Ingrese el numero: " << i + 1 << " maximo 12 numeros"<<endl;
+            cin >> numeros[i];
+    }
+    
+}
